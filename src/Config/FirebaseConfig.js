83c9838930a8b533f7d45af1c5d0de6e43a1a7e5
Harshvardhan Shete <hvshete6@gmail.com>
2021-08-23 21:15:55 +0530

    First commit

---
+++ src/Config/FirebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyCmufAJqCuINCW_UwnbPiom2_7AMLjyMlg",
+  authDomain: "mygitapp-react.firebaseapp.com",
+  projectId: "mygitapp-react",
+  storageBucket: "mygitapp-react.appspot.com",
+  messagingSenderId: "682645721398",
+  appId: "1:682645721398:web:abb3dfb5cf33f142e06397",
+  measurementId: "G-3PX0CXQNLS"
+};
